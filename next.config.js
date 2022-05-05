d77167a690ae33786d26d96bc21ef426a8a557e9
Techoshi <risi@eminent-it.com>
2022-05-04 22:38:25 -0400

    enable Walletconnect

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
     // tokenContractSymbol : "xM",
     tokenContractAddress : "0xEEFE69a45CB83d8e62d4ba22F7068480BE09b78c",
     vaultContractAddres : "0x32db1720afbf5c639291ca274d05bccd92594b7b",
-    enableWalletConnect : false
+    enableWalletConnect : true
   },
 }
 
